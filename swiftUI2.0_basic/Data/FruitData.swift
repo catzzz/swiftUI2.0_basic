ec958256515f076fdbe928a2ba95febdc05743fb
catzzz <chuneleu@gmail.com>
2020-12-16 15:08:33 -0800

    add fruits data

---
+++ swiftUI2.0_basic/Data/FruitData.swift
@@ -0,0 +1,8 @@
+//
+//  FruitData.swift
+//  swiftUI2.0_basic
+//
+//  Created by Jimmy on 12/16/20.
+//
+
+import Foundation
---
+++ swiftUI2.0_basic/Model/FruitModel.swift
@@ -0,0 +1,8 @@
+//
+//  FruitModel.swift
+//  swiftUI2.0_basic
+//
+//  Created by Jimmy on 12/16/20.
+//
+
+import Foundation
