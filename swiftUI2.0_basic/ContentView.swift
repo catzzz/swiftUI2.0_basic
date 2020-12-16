148b4fb4429a0cd930648492e1f86b73014fdbf8
catzzz <chuneleu@gmail.com>
2020-12-16 11:34:46 -0800

    Initial Commit

---
+++ swiftUI2.0_basic/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftUI2.0_basic
+//
+//  Created by Jimmy on 12/16/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftUI2.0_basic/swiftUI2_0_basicApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI2_0_basicApp.swift
+//  swiftUI2.0_basic
+//
+//  Created by Jimmy on 12/16/20.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI2_0_basicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
